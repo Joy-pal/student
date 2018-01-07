1dc64b6098a3382da82112b225d65fc210023774
Joy <Joy@Joy-PC>
2018-01-07 12:29:20 +0600

    first project fro eclips

---
+++ FirstProject/src/MainClass.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.lang.reflect.Array;
+
+import javax.swing.JFrame;
+ 
+public class MainClass {
+
+	public static void main(String[] args) {
+		 
+	//	Integer i=new Integer ();
+		int i=1010;
+		System.out.println(Integer.toBinaryString(i));
+		System.out.println(	Integer.bitCount( i));
+		Integer .
+ 	}
+
+}
